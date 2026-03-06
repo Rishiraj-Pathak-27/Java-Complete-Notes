64b497dfae2819a12db3493f0ad5217a8701bdfd
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-03-06 22:49:07 +0530

    Implement search for Kth missing positive number in sorted array and update search in sorted rotated array

---
+++ arrays/Search/KPositiveMissing.java
@@ -0,0 +1,33 @@
+// WAP to find the Kth missing positive number from sorted array
+
+public class KPositiveMissing {
+
+    public static int positiveMissing(int[] arr, int k){
+        int n=arr.length;
+
+        int left=0, right=n-1;
+
+        while(left<=right){
+            int mid = left+(right-left)/2;
+
+            int missingNo = mid+1;
+            int totalMissing = arr[mid] - missingNo;
+
+            if(totalMissing >= k){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return left+k;
+        // OR
+        // return right+1+k;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1,2,5,7,8};
+        int k = 3;
+
+        System.out.println("Positive Missing number = "+positiveMissing(arr,k));
+    }
+}
--- arrays/Search/searchInSortedRotated.java
+++ arrays/Search/searchInSortedRotated.java
@@ -1,29 +1,68 @@
 // WAP to search an element in a sorted and rotated array using binary search (O(log n))
 
-public class searchInSortedRotated {
+// public class searchInSortedRotated {
+//     public static int sortedRotated(int[] arr, int target){
+//         int n=arr.length;
+
+//         int left = 0, right = n-1;
+
+//         while(left<=right){
+//             int mid = left+(right-left)/2;
+//             if(arr[mid] == target) return mid;
+
+//             // check if the left side is sorted
+//             else if(arr[left] <= arr[mid]){
+
+//                 // is element present in the left half
+//                 if(arr[left] <= target && target < arr[mid]){
+//                     right = mid-1;
+//                 }else{
+//                     left = mid+1;
+//                 }
+
+//                 // check if the right half is sorted 
+//             }else{ // (arr[mid] <= arr[right])
+
+//                 // is element present in the right half
+//                 if(arr[mid] < target && target <= arr[right]){
+//                     left = mid+1;
+//                 }else{
+//                     right = mid-1;
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+//     public static void main(String[] args){
+//         int[] arr = {8,9,10,11,12,1,2,3,4,5,6,7};
+//         int target = 11;
+
+//         System.out.println("We got our target at idx "+sortedRotated(arr,target));
+//     }   
+// }
+
+
+public class searchInSortedRotated{
     public static int sortedRotated(int[] arr, int target){
         int n=arr.length;
 
-        int left = 0, right = n-1;
+        int left=0, right=n-1;
 
         while(left<=right){
             int mid = left+(right-left)/2;
+
             if(arr[mid] == target) return mid;
 
-            // check if the left side is sorted
             else if(arr[left] <= arr[mid]){
-
-                // is element present in the left half
-                if(arr[left] <= target && target < arr[mid]){
+                if(arr[left] <= target && target <= arr[mid]){
                     right = mid-1;
                 }else{
                     left = mid+1;
                 }
-                // check if the right half is sorted 
-            }else{ // (arr[mid] <= arr[right])
-
-                // is element present in the right half
-                if(arr[mid] < target && target <= arr[right]){
+            }
+            
+            else{ // (arr[mid]<=arr[right])
+                if(arr[mid] <= target && target <= arr[right]){
                     left = mid+1;
                 }else{
                     right = mid-1;
@@ -32,10 +71,11 @@ public class searchInSortedRotated {
         }
         return -1;
     }
-    public static void main(String[] args){
-        int[] arr = {8,9,10,11,12,1,2,3,4,5,6,7};
-        int target = 11;
 
-        System.out.println("We got our target at idx "+sortedRotated(arr,target));
-    }   
+    public static void main(String[] args){
+        int[] arr = {8,9,10,1,2,3,4,5,6,7};
+        int target = 8;
+
+        System.out.println(sortedRotated(arr,target));
+    }
 }
