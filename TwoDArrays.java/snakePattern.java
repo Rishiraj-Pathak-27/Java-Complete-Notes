8a6b4f4d54477e3cf4aaf956ad15fabb08b4c366
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-03-09 22:27:19 +0530

    Add snake pattern printing implementation and reverse column-wise printing for 2D arrays

--- TwoDArrays.java/snakePattern.java
+++ TwoDArrays.java/snakePattern.java
@@ -31,7 +31,6 @@
 
 // WAP to print the 2D array in snake pattern column wise
 
-
 public class snakePattern{
 
     public static void snakePrinting(int[][] arr){
@@ -39,26 +38,36 @@ public class snakePattern{
         int n=arr.length;
         int m=arr[0].length;
 
+        int[][] res = new int[n][m];
+
         for(int j=0; j<m; j++){
-            if(j%2==1){
-                for(int i=n-1; i>=0; i--){
-                    System.out.print(arr[i][j]+" ");
+            if(j%2==0){
+                for(int i=0; i<n; i++){
+                    res[i][j] = arr[i][j];
                 }
             }
             else{
-                for(int i=0; i<n; i++){
-                    System.out.print(arr[i][j]+" ");
+                int k=0;
+
+                for(int i=n-1; i>=0; i--){
+                    res[k][j] = arr[i][j];
+                    k++;
                 }
             }
             System.out.println();
         }
 
-}
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(res[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args){
-
-        int[][] arr = {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}};
-
+        int[][] arr = {{2,8,3,4},{7,2,1,6},{5,5,4,1},{3,1,8,2}};
         snakePrinting(arr);
-
     }
 }
