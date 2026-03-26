cb95c2ebbe309cb286d430e1f19fd5031936b345
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-03-26 20:42:12 +0530

    Add implementation to check for prime numbers using the Sieve of Eratosthenes algorithm

--- Exercise/loops/loops.java
+++ Exercise/loops/loops.java
@@ -1007,3 +1007,27 @@ public class loops{
     }
 } */
 
+
+// WAP to check if a number is prime or not T.C. -> O(underoot n)
+
+public class loops{
+    public static void main(String[] args){
+        int n=5;
+
+        int count=0;
+
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n%i==0){
+                count++;
+                break;
+            }
+        }
+
+        if(count==0){
+            System.out.println("Prime number");
+        }else{
+            System.out.println("Not a Prime number");
+        }
+    }
+}
+
---
+++ Platform_Questions/leetcode/countPrimes.java
@@ -0,0 +1,75 @@
+// 204. Count Primes
+
+// Approach 1 (Using T.C. -> O(n underoot n)
+
+// public class countPrimes {
+//     public static int primes(int n){
+//         int count=0;
+//         int countOne=0;
+
+//         int val=(int)(Math.sqrt(n));
+
+//         if(n<=1) return 0;
+
+//         for(int i=1; i<=n; i++){
+//             for(int j=2; j<=val; j++){
+//                 if(n%j==0){
+//                     count++;
+//                     break;
+//                 }
+//             }
+//             if(count==0){
+//                 countOne++;
+//             }
+//         }
+//         return countOne;
+//     }
+
+
+//     public static void main(String[] args){
+//         int n=10;
+
+//         System.out.println(primes(n));
+//     }
+// }
+
+/////////////////////////////////////////////////
+
+
+// Approach 2 (T.C. -> O(n log log n))
+
+public class countPrimes{
+    public static int primes(int n){
+        
+        if(n<=2) return 0;
+        
+        boolean[] flag = new boolean[n];
+        
+        for(int i=2; i<n; i++){
+            flag[i] = true;
+        }
+
+        for(int i=2; i<Math.sqrt(n); i++){
+            if(flag[i]){
+                for(int j=i*i; j<(n); j+=i){
+                    flag[j] = false;
+                }
+            }
+        }
+
+        int count=0;
+        for(int i=2; i<n; i++){
+            if(flag[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int n = 12;
+
+        System.out.println(primes(n));
+    }
+
+}
