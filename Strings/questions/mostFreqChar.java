103d34dd61b9293c4e59a155aa2bf8e1cc4cdadd
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-04-07 22:42:43 +0530

    Add mostFreqChar.java to implement a method for finding the most frequent character in a string

---
+++ Strings/questions/mostFreqChar.java
@@ -0,0 +1,87 @@
+// WAP to find out the most frequent character from the string
+
+// Approach 1 (Brute Force T.C. -> (O(N^2)))
+
+/* public class mostFreqChar{
+
+    public static char mostFrequent(String str){
+        int n=str.length();
+        int maxCount=0;
+        char ans = str.charAt(0);
+
+        // outer loop used to traverse the whole string
+        for(int i=0; i<n; i++){
+            char ch = str.charAt(i);
+            int count=0;
+
+            // inner loop traverse & compare the each char with ith char
+            for(int j=i+1; j<n; j++){
+                if(str.charAt(j) == ch){
+                    count++;        // used to store count at each jth iteration if ch founds
+                }
+            }
+
+            // USED to handle the if max founds
+            if(count>maxCount){
+                maxCount = count;
+                ans = ch;
+            }else if(count == maxCount && ans > ch){
+                ans = ch;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        String s = "testsample";
+
+        System.out.println("Most frequent character = "+mostFrequent(s));
+    }
+} */
+
+// Approach 2 (Sorting Approach)
+
+import java.util.Arrays;
+public class mostFreqChar {
+
+    public static char mostFrequent(String s){
+        
+        char[] arr = s.toCharArray();
+        int n=arr.length;
+
+        // 1) sort the array
+        Arrays.sort(arr);
+
+        int maxCount=-1;
+        char ans = arr[0];
+
+        int i=0, j=0;
+
+        while(j<n){
+            if(arr[i] == arr[j]){
+                j++;
+            }else{
+                int count = j-i;
+                if(count>maxCount) {
+                    maxCount = count;
+                    ans = arr[i];
+                }
+                i=j;
+            }
+        }
+
+        // this is used to handle the last sequence of characters as after j>n the loop breaks these things are handled outside
+        int count=j-i;
+        if(maxCount<count){
+            maxCount=count;
+            ans=arr[i];
+        }
+        i=j;
+        return ans;
+    }
+
+    public static void main(String[] args){
+        String str = "character";    // aaccehrrt
+        System.out.println("Most frequent character = "+mostFrequent(str));
+    } 
+}
