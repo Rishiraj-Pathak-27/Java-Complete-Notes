f74136762b17230b4cab0cf1bb3a149cdb63f8e1
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-04-13 20:24:03 +0530

    Add implementation for finding the first unique character in a string

---
+++ Strings/questions/nonRepeatingChar.java
@@ -0,0 +1,34 @@
+// 387. First Unique Character in a String
+// Approach 1) Optimal T.C -> (O(N)) S.C -> (O(1))
+
+public class nonRepeatingChar {
+
+    public static int nonRepeating(String s){
+        int n=s.length();
+        int[] arr = new int[26];
+
+        for(int i=0; i<n; i++){
+            char ch = s.charAt(i);
+            int idx = ch-97;
+
+            arr[idx]++;
+        }
+
+        
+        for(int i=0; i<26; i++){
+            char minChar=s.charAt(i);
+            int idx = minChar-97;
+
+            if(arr[idx] == 1){
+                return i;
+            }
+        }
+        return ' ';
+    }
+
+    public static void main(String[] args){
+        String s = "geeksforgeeks";
+
+        System.out.println(nonRepeating(s));
+    }
+} 
