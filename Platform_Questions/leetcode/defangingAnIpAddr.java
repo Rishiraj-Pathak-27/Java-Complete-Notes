93d3031a4934fbe9352ff695f2355e3ba667a760
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-05-17 23:42:30 +0530

    Add implementation for defanging an IP address

---
+++ Platform_Questions/leetcode/defangingAnIpAddr.java
@@ -0,0 +1,10 @@
+public class defangingAnIpAddr {
+    public static String defangIPaddr(String address) {
+        return address.replace(".","[.]");
+    }
+
+    public static void main(String[] args){
+        String address = "1.1.1.1.1.1.1";
+        System.out.println(defangIPaddr(address));
+    }
+}
