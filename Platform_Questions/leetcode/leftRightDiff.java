3e1c8cff134a0e7f0f751828b2a9542137c967b7
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-05-18 21:06:45 +0530

    Add implementation for left and right sum differences

---
+++ Platform_Questions/leetcode/leftRightDiff.java
@@ -0,0 +1,29 @@
+// 2574. Left and Right Sum Differences
+
+import java.util.Arrays;
+public class leftRightDiff {
+
+    public static int[] lRDiff(int[] arr){
+        int n=arr.length;
+        int leftSum=0;
+        int rightSum=0;
+
+        for(int ele:arr){
+            rightSum+=ele;
+        }
+
+        int[] res = new int[n];
+        for(int i=0; i<n; i++){
+            leftSum+=arr[i];
+            res[i] = Math.abs(rightSum-leftSum);
+            rightSum=rightSum-arr[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {10,4,8,3};
+        int[] res = lRDiff(arr);
+        System.out.println("Ans = "+Arrays.toString(res));
+    } 
+}
