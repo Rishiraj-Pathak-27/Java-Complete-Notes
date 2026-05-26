7f2c546b66456bde326fd3e7742480867be92b16
Rishiraj-Pathak-27 <rishirajraj124@gmail.com>
2026-05-26 22:52:08 +0530

    Implement SpiralMatrixII class to generate a spiral matrix and add selfDividingNumbers class for identifying self-dividing numbers

---
+++ Platform_Questions/leetcode/selfDividingNumbers.java
@@ -0,0 +1,96 @@
+// 728. Self Dividing Numbers
+// import java.util.List;
+// import java.util.ArrayList;
+// public class Solution {
+
+//     public static int count(int n){
+//         int count=0;
+
+//         while(n!=0){
+//             int rem=n%10;
+//             count++;
+//             n/=10;
+//         }
+//         return count;
+//     }
+
+//     public static boolean helper(int n){
+//         int copy=n;
+//         int count=0;
+//         int digits=count(n);
+
+
+//         while(n!=0){
+//             int rem = n%10;
+
+//             if(rem==0) return false;
+
+//             if(copy%rem==0){
+//                 count++;
+//             }
+//             n/=10;
+//         }
+//         if(count==digits){
+//             return true;
+//         }
+//         return false;
+//     }
+
+
+//     public List<Integer> selfDividingNumbers(int left, int right) {
+
+//         List<Integer> ans = new ArrayList<>();
+
+//         for(int i=left; i<=right; i++){
+//             if(helper(i)){
+//                 ans.add(i);
+//             }
+//         }
+//         return ans;
+//     }
+    // public static void main(String[] args) {
+    //     int left=1, right=22;
+    //     System.out.println(selfDividingNumbers(left,right));
+    // }
+// }
+
+// OR
+
+import java.util.ArrayList;
+import java.util.List;
+public class selfDividingNumbers {
+
+    public static boolean helper(int n){
+        int copy=n;
+        int count=0;
+
+        while(n!=0){
+            int rem = n%10;
+
+            if(rem==0 || copy%rem!=0){
+                return false;
+            }
+            n/=10;
+        }
+        return true;
+    }
+
+
+    public static List<Integer> selfDividingNumbers(int left, int right) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        for(int i=left; i<=right; i++){
+            if(helper(i)){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        int left=1, right=22;
+        System.out.println(selfDividingNumbers(left,right));
+    }
+}
